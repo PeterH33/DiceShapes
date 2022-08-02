c887ab243dbafbc069ba0983b87588cae59bbac2
Peter Hartnett <peter.hartnett@gmail.com>
2022-08-02 13:44:25 -0400

    Initial Commit

---
+++ Shapes/ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesApp.swift
+//  Shapes
+//
+//  Created by Peter Hartnett on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
